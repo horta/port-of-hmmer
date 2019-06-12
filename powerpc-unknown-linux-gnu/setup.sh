0742ead6bcfeb2a384d83e2ce3ccda1293e622c5
Danilo Horta <danilo.horta@gmail.com>
2019-06-12 21:45:00 +0100

    First commit

---
+++ powerpc-unknown-linux-gnu/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DIR=/hostdir
+
+touch nohup.out
+nohup /usr/bin/qemu-system-ppc -nographic -vga none -L bios \
+    -hda ./debian-wheezy-powerpc.qcow2 -m 512M -net user,hostfwd=tcp::22125-:22 \
+    -virtfs local,path=$DIR,mount_tag=host0,security_model=passthrough,id=host0 \
+    -net nic >nohup.out 2>&1 &
+
+tail -f nohup.out | tee /dev/tty | while read LOGLINE
+do
+    [[ "${LOGLINE}" == *"Debian GNU/Linux 7 debian-powerpc"* ]] && pkill -P $$ tail
+done
+
+echo "Done."
+
+ssh_cmd () {
+    sshpass -p 'root' ssh -t -oStrictHostKeyChecking=no 127.0.0.1 -p 22125 -l root "$1"
+}
+
+ssh_cmd "echo \"host0 $DIR 9p trans=virtio,version=9p2000.L   0 0\" >> /etc/fstab"
+ssh_cmd "mkdir $DIR"
+ssh_cmd "mount $DIR"
+ssh_cmd "ls $DIR"
