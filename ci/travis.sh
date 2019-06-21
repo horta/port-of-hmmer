be2e95be47ce44e95e53f492c75202a077b83f63
Danilo Horta <danilo.horta@gmail.com>
2019-06-21 18:28:55 +0100

    qemu directly

--- ci/travis.sh
+++ ci/travis.sh
@@ -2,30 +2,35 @@
 
 set -e
 
-if [ "$TRAVIS_OS_NAME" == "linux" ]
-then
-  IMAGE_NAME=hortaebi/port-of-hmmer:$TARGET
-
-  if [ "${BUILD_IMAGE+x}" = "x" ] && [ "$BUILD_IMAGE" == "true" ]
-  then
-    (cd $TARGET && docker build -t $IMAGE_NAME .)
-  else
-    docker pull $IMAGE_NAME
-  fi
-
-  docker run -dit -v $TRAVIS_BUILD_DIR:/hostdir --name $TARGET $IMAGE_NAME
-
-  until [ "`docker inspect -f {{.State.Health.Status}} $TARGET`" == "healthy" ]
-  do
-    sleep 30
-    echo "Waiting for docker service to finish startup..."
-  done
-
-  echo "Docker service startup has finished!"
-fi
+HOSTDIR=/hostdir
 
 mkdir $HOME/bin || true
-
 curl https://raw.githubusercontent.com/horta/port-of-hmmer/master/ci/sandbox_run \
     --output $HOME/bin/sandbox_run
 chmod +x $HOME/bin/sandbox_run
+hash -r
+
+if [ "$TARGET" == "powerpc-unknown-linux-gnu" ]
+then
+  sudo apt-get install -y qemu qemu-system-ppc openssh-clients sshpass
+
+  curl http://rest.s3for.me/hmmer/debian-wheezy-powerpc.qcow2 \
+    --output debian-wheezy-powerpc.qcow2
+
+  touch nohup.out
+  nohup qemu-system-ppc -nographic -vga none -L bios \
+      -hda ./debian-wheezy-powerpc.qcow2 -m 512M -net user,hostfwd=tcp::22125-:22 \
+      -virtfs local,path=$HOSTDIR,mount_tag=host0,security_model=passthrough,id=host0 \
+      -net nic >nohup.out 2>&1 &
+  
+  tail -f nohup.out | tee /dev/tty | while read LOGLINE
+  do
+    [[ "${LOGLINE}" == *"Debian GNU/Linux 7 debian-powerpc"* ]] && pkill -P $$ tail
+  done
+
+  echo "PPC machine has started."
+
+  sandbox_run "echo \"host0 $HOSTDIR 9p trans=virtio,version=9p2000.L   0 0\" >> /etc/fstab"
+  sandbox_run "mkdir -p $HOSTDIR"
+  sandbox_run "mount $HOSTDIR"
+fi
