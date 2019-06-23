64013e8d79dac2fdf0d79beebf5394ee3438cb2a
Danilo Horta <danilo.horta@gmail.com>
2019-06-23 23:56:32 +0100

    id

--- ci/travis.sh
+++ ci/travis.sh
@@ -50,13 +50,12 @@ ppc_setup()
 
   ppc_run ()
   {
-    echo $@
     sshpass -p "root" ssh -t -oStrictHostKeyChecking=no 127.0.0.1 -p 22125 -l root "$@"
   }
   
   ppc_run groupadd -g $grp_id $usr_name
   ppc_run useradd -u $usr_id -g $grp_id -m $usr_name
-  ppc_run pam-auth-update
+  # ppc_run pam-auth-update
   sshpass -p "root" ssh -t -oStrictHostKeyChecking=no 127.0.0.1 -p 22125 -l root "echo $usr_name:$usr_name | chpasswd"
 
   echo "PPC setup is done."
