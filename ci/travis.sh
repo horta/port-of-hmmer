c74e39a41d3669e4fb875d940be3f7f2764170de
Danilo Horta <danilo.horta@gmail.com>
2019-06-24 09:52:12 +0100

    opts

--- ci/travis.sh
+++ ci/travis.sh
@@ -58,7 +58,8 @@ ppc_setup()
   ppc_run "echo $usr_name:$usr_name | chpasswd"
   ppc_run "chmod 777 /hostdir"
   ppc_run "chown $usr_name:$usr_name /hostdir"
-  ppc_run "mount -t 9p -o uid=$usr_id,gid=$grp_id,umask=000,trans=virtio,version=9p2000.L host0 /hostdir"
+  opts="uid=$usr_id,gid=$grp_id,umask=000,trans=virtio,version=9p2000.L"
+  ppc_run "mount -t 9p -o $opts host0 /hostdir"
 
 
   echo "PPC setup is done."
